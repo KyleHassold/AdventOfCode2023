3fd0090af874824d6e6f5838b4dba8f7e19ca6a0
Kyle Hassold <hassold.kyle@gmail.com>
2023-12-02 01:39:26 -0500

    Day 3 prep

---
+++ day3/unknown.py
@@ -0,0 +1,28 @@
+### Imports ###
+
+from time import time
+import re
+
+
+### Methods ###
+
+def helper(data):
+    pass
+
+
+### Solve Puzzle ### 
+
+if __name__ == '__main__':
+    print("Solving Day 3: ...\n")
+    start = time()
+
+    part1_sol = 0
+    part2_sol = 0
+
+    with open('./day3/input.txt', 'r') as f:
+        for l in f:
+            pass
+
+    print('Solved in {:.2f}ms'.format((time()-start)*1000))
+    print('Part 1', part1_sol)
+    print('Part 2', part2_sol)
