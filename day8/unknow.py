a103ad7d73edb91844ba5808ee279dab9a31a8d0
Kyle Hassold <hassold.kyle@gmail.com>
2023-12-04 23:25:14 -0500

    Set up 25 days

---
+++ day8/unknow.py
@@ -0,0 +1,28 @@
+### Imports ###
+
+from time import time
+import re
+
+### Methods ###
+
+def helper(data: str):
+    pass
+
+
+### Solve Puzzle ### 
+
+if __name__ == '__main__':
+    print("Solving Day 8: ...\n")
+    start = time()
+
+    part1_sol = 0
+    part2_sol = 0
+
+    lines = []
+    with open('./day8/input.txt', 'r') as f:
+        for i, l in enumerate(f):
+            pass
+
+    print('Solved in {:.2f}ms'.format((time()-start)*1000))
+    print('Part 1', part1_sol)
+    print('Part 2', part2_sol)
